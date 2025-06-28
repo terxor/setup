5081301269e997498cc15cad19429783978cb377
terxor <39589847+terxor@users.noreply.github.com>
2025-06-29 00:07:37 +0530

    update

--- install/packages-extn.sh
+++ install/packages-extn.sh
@@ -3,10 +3,29 @@
 set -euo pipefail
 
 install_package() {
-  sudo apt-get install -y $1
+  sudo apt install -y $1
 }
 
 sudo apt update -y
+
+
+install_package sway
+install_package wl-clipboard
+install_package waybar
+install_package wofi
+install_package lm-sensors
+install_package swaylock
+
+# Image related tools
+install_package grim
+install_package slurp
+
+install_package brightnessctl
+
+KEYMAPPER_PACKAGE=/tmp/keymapper.deb
+curl -fsSL -o $KEYMAPPER_PACKAGE https://github.com/houmain/keymapper/releases/download/4.12.1/keymapper-4.12.1-Linux-x86_64.deb
+[ "$(shasum -a 256 $KEYMAPPER_PACKAGE | awk '{print $1}')" = "a77a58e0eadb81a245065de992f855c6cfdd244300c0eb64f3daf94412998085" ] && echo "Verified package" || exit 1
+install_package $KEYMAPPER_PACKAGE
 
 # GUI extn.
 # install_package i3blocks
--- install/packages.sh
+++ install/packages.sh
@@ -3,21 +3,22 @@
 set -euo pipefail
 
 install_package() {
-  sudo apt-get install -y $1
+  sudo apt install -y $1
 }
 
 sudo apt update -y
 
 # Base packages
-install_package xorg
-install_package lightdm
-install_package i3-wm
+# install_package xorg
+# install_package lightdm
+# install_package i3-wm
+#
 install_package alacritty
 
 # Dev tools
 install_package python3.13
-# install_package g++
-# install_package build-essential
+install_package g++
+install_package build-essential
 install_package git
 
 # Generic utility
@@ -27,7 +28,7 @@ install_package xclip
 install_package curl
 install_package stow
 install_package unzip
-# install_package ripgrep
+install_package ripgrep
 # install_package vim-gtk3
 
 # Terminal
